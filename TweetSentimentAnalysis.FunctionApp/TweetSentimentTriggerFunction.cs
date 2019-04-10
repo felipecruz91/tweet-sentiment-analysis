90c5ef3c3ff95808429434f83f62646bf2c5e0ce
Felipe Cruz <fcz@kamstrup.com>
2019-04-10 20:24:30 +0200

    Initial work on trying to connect to SignalR from the Azure Function Trigger

--- TweetSentimentAnalysis.FunctionApp/TweetSentimentTriggerFunction.cs
+++ TweetSentimentAnalysis.FunctionApp/TweetSentimentTriggerFunction.cs
@@ -1,26 +1,65 @@
-using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Microsoft.Extensions.Logging;
 
 namespace TweetSentimentAnalysis.FunctionApp
 {
     public static class TweetSentimentTriggerFunction
     {
+        /// <summary>
+        ///     This function will be called by our client to retrieve the SignalR Service endpoint.
+        /// </summary>
+        /// <param name="req"></param>
+        /// <param name="connectionInfo"></param>
+        /// <returns></returns>
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo GetSignalRInfo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")]
+            HttpRequest req,
+            [SignalRConnectionInfo(HubName = "tweetsHub")]
+            SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+
         [FunctionName("TweetSentimentTrigger")]
-        public static void Run([CosmosDBTrigger(
-            databaseName: "TweetAnalysis",
-            collectionName: "TweetSentiment",
-            ConnectionStringSetting = "CosmosDbConnectionString",
-            CreateLeaseCollectionIfNotExists = true,
-            LeaseCollectionName = "leases")]IReadOnlyList<Document> documents, TraceWriter log)
+        public static Task Run([CosmosDBTrigger(
+                "TweetAnalysis",
+                "TweetSentiment",
+                ConnectionStringSetting = "CosmosDbConnectionString",
+                MaxItemsPerInvocation = 5,
+                CreateLeaseCollectionIfNotExists = true,
+                LeaseCollectionName = "leases")]
+            IReadOnlyList<Document> documents
+            , ILogger log
+            , [SignalR(HubName = "tweetsHub")] IAsyncCollector<SignalRMessage> signalRMessages)
         {
             if (documents != null && documents.Count > 0)
             {
-                log.Verbose("Documents modified " + documents.Count);
-                log.Verbose("First document Id " + documents[0].Id);
+                log.LogInformation("Documents modified " + documents.Count);
+
+                foreach (var document in documents)
+                {
+                    log.LogInformation("Document Id " + document.Id);
+
+                    var fullText = document.GetPropertyValue<string>("fullText");
+                    var score = document.GetPropertyValue<double>("score");
+
+                    return signalRMessages.AddAsync(
+                        new SignalRMessage
+                        {
+                            Target = "newMessage",
+                            Arguments = new[] {$"Text: {fullText}. Score: {score}"}
+                        });
+                }
             }
+
+            return null;
         }
     }
 }
