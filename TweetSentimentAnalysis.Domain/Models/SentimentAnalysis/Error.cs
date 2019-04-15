c7323c9ee33c41c10f5650d69f9cd3b727067277
Jesus Estevez <jecaestevez@outlook.com>
2019-04-15 23:52:27 +0200

    improve handling http response errors from cognitive services

--- TweetSentimentAnalysis.Domain/Models/SentimentAnalysis/Error.cs
+++ TweetSentimentAnalysis.Domain/Models/SentimentAnalysis/Error.cs
@@ -5,6 +5,7 @@ namespace TweetSentimentAnalysis.Domain.Models.SentimentAnalysis
     public class Error
     {
         [JsonProperty("id")] public string Id { get; set; }
+        [JsonProperty("code")] public string Code { get; set; }
         [JsonProperty("message")] public string Message { get; set; }
     }
 }
