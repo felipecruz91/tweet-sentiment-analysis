59392d0ef27cf93243844d4fb8993d1e5e5ee77b
Felipe Cruz <fcz@kamstrup.com>
2019-04-25 19:23:20 +0200

    'TextAnalyticsConfiguration' was not implementing interface member 'ITextAnalyticsConfiguration.Language'

--- TweetSentimentAnalysis.Processor/TextAnalyticsConfiguration.cs
+++ TweetSentimentAnalysis.Processor/TextAnalyticsConfiguration.cs
@@ -15,5 +15,6 @@ namespace TweetSentimentAnalysis.Processor
         public string Name => _configuration["TextAnalytics:Name"];
         public string Key1 => _configuration["TextAnalytics:Key1"];
         public string Key2 => _configuration["TextAnalytics:Key2"];
+        public string Language => _configuration["TextAnalytics:Language"];
     }
 }
